eb0752e006d69d1e4ee808a96a5b60b7a09e7726
Diego-Carvalho2005 <diegocarvalho3202@gmail.com>
2025-06-08 16:41:35 -0300

    Created Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.time.LocalDateTime;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(24, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
