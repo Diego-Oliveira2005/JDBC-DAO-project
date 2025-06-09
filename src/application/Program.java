3c1568bfd69438228d1137a184bd485ccdcc64f7
Diego-Carvalho2005 <diegocarvalho3202@gmail.com>
2025-06-09 17:36:03 -0300

    Created SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import models.dao.DaoFactory;
+import models.dao.SellerDao;
 import models.entities.Department;
 import models.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(24, "Bob", "bob@gmail.com", LocalDateTime.now(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
